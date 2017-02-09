9e1df26831e00d38715e23cc876fd6d222e5e891
Patrick van der Leer <pat.vdleer@gmail.com>
2017-02-09 18:44:35 +0100

    Updated README.md and added cli interface based on robertklep/nefit-easy-cli

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# nefit-client-python
+# Nefit Client
+
+## Usage
+```bash
+usage: Nefit client [options]
+
+Nefit client
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --serial SERIAL       Serial, 9 number digit
+  --access-key ACCESS_KEY
+                        Access key, 12 long
+  --password PASSWORD   Password, usually postalcode + housenumber
+  --status              Status
+  --display-code        Display code
+  --set-temperature SET_TEMPERATURE
+                        Display code
+  --version             show program's version number and exit
+```
---
+++ cli.py
@@ -0,0 +1,66 @@
+import time
+import sys
+import signal
+
+from nefit import NefitClient
+
+try:
+    import argparse
+except ImportError:
+    print("The Python module argparse is required")
+    sys.exit(1)
+
+
+class CLI:
+    nefit_client = None
+
+    @staticmethod
+    def sig_handler(signum=None, frame=None):
+        if signum is not None:
+            pass
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.sig_handler)
+        signal.signal(signal.SIGTERM, self.sig_handler)
+
+        self.parser = parser = argparse.ArgumentParser(
+            description='Nefit client',
+            prog="Nefit client",
+            usage='%(prog)s [options]'
+        )
+        parser.add_argument("--serial", help='Serial, 9 number digit')
+        parser.add_argument("--access-key", dest="access_key", help='Access key, 12 long')
+        parser.add_argument("--password", help='Password, usually postalcode + housenumber')
+        parser.add_argument("--status", help="Status", action="store_true")
+        parser.add_argument("--display-code", dest="display_code", help="Display code", action="store_true")
+        parser.add_argument("--set-temperature", dest="set_temperature", help="Display code", type=float)
+        # parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
+        parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+
+    def parse(self, args=None):
+        args = self.parser.parse_args(args)
+        return args
+
+    def run(self):
+        args = self.parse()
+        client = NefitClient(args.serial, args.access_key, args.password)
+        client.connect()
+
+        if args.status:
+            for k, v in client.get_status().items():
+                if isinstance(v, bool):
+                    print("%s: %s" % (k, "Yes" if v else "No"))
+                print("%s: %s" % (k, v))
+
+        if args.display_code:
+            print(client.get_display_code())
+
+        if args.set_temperature:
+            client.set_temperature(args.set_temperature)
+            print("Temperature set to %f" % args.set_temperature)
+
+        client.disconnect()
+
+if __name__ == "__main__":
+    cli = CLI()
+    cli.run()
