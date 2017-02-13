4879015250811dbdc6913a62ea813aca8afc1cad
Patrick van der Leer <pat.vdleer@gmail.com>
2017-02-13 12:44:51 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='nefit-client',
+    version='0.1',
+    description='Python API and command line tool for talking to the Nefit Easy™ Thermostat',
+    author='Patrick van der Leer',
+    author_email='pat.vdleer@gmail.com',
+    maintainer='Patrick van der Leer',
+    maintainer_email='pat.vdleer@gmail.com',
+    url='https://github.com/patvdleer/nefit-client-python.git',
+    scripts=['nefit.py'],
+    install_requires=['pycrypto', 'sleekxmpp']
+)
