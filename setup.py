8124898155e82ebe493eeae19efb19a1588931c8
Marco Reichwald <marconfus@gmail.com>
2017-04-22 19:55:33 +0200

    Rework into module

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 
 setup(
     name='nefit-client',
-    version='0.1',
+    version='0.2',
     description='Python API and command line tool for talking to the Nefit Easy™ Thermostat',
     author='Patrick van der Leer',
     author_email='pat.vdleer@gmail.com',
     maintainer='Patrick van der Leer',
     maintainer_email='pat.vdleer@gmail.com',
     url='https://github.com/patvdleer/nefit-client-python.git',
-    scripts=['nefit.py'],
+    packages=["nefit"],
     install_requires=['pycrypto', 'sleekxmpp']
 )
