e9a8f71a329b63d75ec2a8f7326aa38c16e8e1c9
Patrick van der Leer <pat.vdleer@gmail.com>
2017-04-28 10:38:47 +0200

    Added check for README.md file in setup

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@
 # -*- coding:utf-8 -*-
 
 from setuptools import setup
+import os
 
 setup(
     name='nefit-client',
-    version='0.2',
+    version='0.2.1',
     description='Python API and command line tool for talking to the Nefit Easy™ Thermostat',
-    long_description=open("README.md").read(),
+    long_description=open("README.md").read() if os.path.exists("README.md") else "",
     author='Patrick van der Leer',
     author_email='pat.vdleer@gmail.com',
     maintainer='Patrick van der Leer',
     maintainer_email='pat.vdleer@gmail.com',
     url='https://github.com/patvdleer/nefit-client-python.git',
+    download_url='https://github.com/patvdleer/nefit-client-python/archive/v0.2.1.tar.gz',
     packages=["nefit"],
     install_requires=['pyaes', 'sleekxmpp']
 )
