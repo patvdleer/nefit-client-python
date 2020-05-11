081b167f7829c4879e26d23707d6a9e65fed35c6
Patrick van der Leer <pat.vdleer@gmail.com>
2020-05-11 11:37:08 +0200

    Fixes setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     version=version,
     description='Python API and command line tool for talking to the Nefit Easy™ Thermostat',
     long_description=open("README.md").read() if os.path.exists("README.md") else "",
+    long_description_content_type="text/markdown",
     author='Patrick van der Leer',
     author_email='pat.vdleer@gmail.com',
     maintainer='Patrick van der Leer',
